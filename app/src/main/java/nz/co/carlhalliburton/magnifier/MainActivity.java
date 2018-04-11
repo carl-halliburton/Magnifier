234cc7efe7ef7f4d9466d2afd56db2c5f55f1e37
carl-halliburton <carl.j.halliburton@gmail.com>
2018-04-11 15:17:37 +1200

    added updated launcher icon

--- app/src/main/java/nz/co/carlhalliburton/magnifier/MainActivity.java
+++ app/src/main/java/nz/co/carlhalliburton/magnifier/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setNavigationIcon(R.mipmap.ic_launcher);
         setSupportActionBar(toolbar);
     }
